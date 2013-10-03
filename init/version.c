5f635c9ea3feea439aa843def0610ea5d5e6a590
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-10-03 20:46:41 +0200

    Bump kernel version to 0.508_cwm_swappedSD
    Change-Id: I47a1c41aa28a40a24ea075a56a69fb29476253bb

--- init/version.c
+++ init/version.c
@@ -44,5 +44,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (Mackay_kernel_0.508_cwm)"
+	" (Mackay_kernel_0.508_cwm_swappedSD)"
 	" (" LINUX_COMPILER ") %s\n";
